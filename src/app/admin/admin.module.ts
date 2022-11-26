3c1aa876a444f6ae9e18296b7fb3ed6bf80030d6
abdelraheem4 <aarabiyyat85@gmail.com>
2022-11-26 17:54:26 +0300

    final

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { ManageUserComponent } from './manage-user/manage-user.component';
 import { SharedModule } from '../shared/shared.module';
+import { ManageUserComponent } from './manage-user/manage-user.component';
 
 
 @NgModule({
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,7 +25,8 @@ const routes: Routes = [ {
 {
   path:'admin',
   loadChildren:()=>AdminModule
-}
+},
+
 
 
 ];
