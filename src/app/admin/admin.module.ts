5404dba74d5c97e4b6a971a96eeeb3eaa95bf8a5
abdelraheem4 <aarabiyyat85@gmail.com>
2022-11-28 00:41:19 +0300

    ficed

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,33 +5,25 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { SharedModule } from '../shared/shared.module';
 import { ManageUserComponent } from './manage-user/manage-user.component';
-import { ManagevaccinesComponent } from './managevaccines/managevaccines.component';
 import { VaccinaiationCenterComponent } from './vaccinaiation-center/vaccinaiation-center.component';
 import { TestmonialComponent } from './testmonial/testmonial.component';
-<<<<<<< HEAD
-=======
 import { CreateUserComponent } from './create-user/create-user.component';
 import { CreatevaccineComponent } from './vaccine/createvaccine/createvaccine.component';
->>>>>>> d55d2c99d6dadeeb2b1969fc5b330947e8ad984f
+import { ManagevaccinesComponent } from './vaccine/managevaccines/managevaccines.component';
 
 
 @NgModule({
   declarations: [
 SidebarComponent,
 ManageUserComponent,
-ManagevaccinesComponent,
-ManagevaccinesComponent,
     SidebarComponent,
     ManageUserComponent,
     TestmonialComponent,
-<<<<<<< HEAD
-    VaccinaiationCenterComponent
-=======
+    VaccinaiationCenterComponent,
     VaccinaiationCenterComponent,
     CreateUserComponent,
-    CreatevaccineComponent
->>>>>>> d55d2c99d6dadeeb2b1969fc5b330947e8ad984f
-  
+    CreatevaccineComponent,
+    ManagevaccinesComponent
   ],
   imports: [
     CommonModule,
