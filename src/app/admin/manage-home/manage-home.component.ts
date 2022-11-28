a959a9c6b02d41f8c5b19035c939537ead7bb6e1
mlklk-22 <mlkmsbh84@gmail.com>
2022-11-28 20:13:33 +0300

    mlk

--- src/app/admin/manage-home/manage-home.component.ts
+++ src/app/admin/manage-home/manage-home.component.ts
@@ -81,40 +81,75 @@ export class ManageHomeComponent implements OnInit {
     this.home.uploadLogoImage(formdata);
   }
   updateform :FormGroup= new FormGroup({
-    idtest:new FormControl(),
-    message:new FormControl('', [Validators.required]),
-    userid:new FormControl()
+         homeid: new FormControl(),
+         imagelogo: new FormControl('', [Validators.required]),
+         littletitle1: new FormControl('', [Validators.required]),
+         title1 : new FormControl('', [Validators.required]),
+         descriptiontitle1 : new FormControl('', [Validators.required]),
+         imagetitle1 : new FormControl('', [Validators.required]),
+         textbutton : new FormControl('', [Validators.required]),
+         statisticstitle : new FormControl('', [Validators.required]),
+         descriptionstatistics : new FormControl('', [Validators.required]),
+         desc1statistics : new FormControl('', [Validators.required]),
+         desc2statistics : new FormControl('', [Validators.required]),
+         desc3statistics : new FormControl('', [Validators.required]),
+         newstitle : new FormControl('', [Validators.required]),
+         newsimage1 : new FormControl('', [Validators.required]),
+         newsimage2 : new FormControl('', [Validators.required]),
+         newsimage3 : new FormControl('', [Validators.required]),
+         newsdesc1 : new FormControl('', [Validators.required]),
+         newsdesc2 : new FormControl('', [Validators.required]),
+         newsdesc3 : new FormControl('', [Validators.required])
   })
-  counter:number = 0;
 
   openCreateDialog(){
     this.dialog.open(this.callCrateTest);
    }
    saveData(){
-     this.home.createTestmonial(this.createform.value);
+     this.home.createHome(this.createform.value);
    }
    p_data:any = {}; 
    openUpdateDialog(obj:any){
      this.p_data={
-       idtest:obj.idtest,
-       message:obj.message,
-       userid:obj.userid
+         homeid: obj.homeid,
+         imagelogo: obj.imagelogo,
+         littletitle1: obj.littletitle1,
+         title1 : obj.title1,
+         descriptiontitle1 :obj.descriptiontitle1,
+         imagetitle1 : obj.imagetitle1,
+         textbutton : obj.textbutton,
+         statisticstitle : obj.statisticstitle,
+         descriptionstatistics : obj.descriptionstatistics,
+         desc1statistics : obj.desc1statistics,
+         desc2statistics : obj.desc2statistics,
+         desc3statistics : obj.desc3statistics,
+         newstitle : obj.newstitle,
+         newsimage1 : obj.newsimage1,
+         newsimage2 : obj.newsimage2,
+         newsimage3 : obj.newsimage3,
+         newsdesc1 : obj.newsdesc1,
+         newsdesc2 : obj.newsdesc2,
+         newsdesc3 : obj.newsdesc3
      }
-     this.updateform.controls['idtest'].setValue(this.p_data.idtest);
-     this.updateform.controls['userid'].setValue(this.p_data.userid);
+     this.updateform.controls['homeid'].setValue(this.p_data.homeid);
+     this.updateform.controls['imagetitle1'].setValue(this.p_data.imagetitle1);
+     this.updateform.controls['newsimage1'].setValue(this.p_data.newsimage1);
+     this.updateform.controls['newsimage2'].setValue(this.p_data.newsimage2);
+     this.updateform.controls['newsimage3'].setValue(this.p_data.newsimage3);
+     this.updateform.controls['imagelogo'].setValue(this.p_data.imagelogo);
  
      this.dialog.open(this.callUpdateTest);
    }
    
    saveupdateData(){
-     this.home.updateTestmonial(this.updateform.value);
+     this.home.updateHome(this.updateform.value);
    }
    openDeleteDialog(id:number){
      const dialogRef = this.dialog.open(this.callDeleteTest);
      dialogRef.afterClosed().subscribe((res)=>{
          if(res != undefined){
            if(res == 'yes')
-           this.home.deleteTestmonial(id);
+           this.home.deleteHome(id);
            else
            console.log('Good Choice');
          }
