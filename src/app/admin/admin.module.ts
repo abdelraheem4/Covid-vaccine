458ef23dda5b0aa4c9a92235ea6e0fe51e116451
mlklk-22 <mlkmsbh84@gmail.com>
2022-11-28 23:40:42 +0300

    mlk

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AdminRoutingModule } from './admin-routing.module';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatDialogModule} from '@angular/material/dialog';
 import { SharedModule } from '../shared/shared.module';
 import { ManageUserComponent } from './manage-user/manage-user.component';
 import { VaccinaiationCenterComponent } from './vaccinaiation-center/vaccinaiation-center.component';
@@ -30,7 +35,14 @@ ManageUserComponent,
   imports: [
     CommonModule,
     AdminRoutingModule,
-    SharedModule
+    SharedModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatDialogModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule
   ]
 })
 export class AdminModule { }
