854b77c1eb39906dd7ff9490ec163ae23bb9932a
sabaqadri <sabatrad63@gmail.com>
2022-12-06 22:20:07 +0300

    saba

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -1,14 +1,16 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { HomeService } from './home.service';
 import jwt_decode from "jwt-decode";
+import { Token } from '@angular/compiler';
+import { Route, Router } from '@angular/router';
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-<<<<<<< HEAD
 
   constructor(private home:HomeService,private toster:ToastrService,private http:HttpClient,private router:Router ) { }
   submit(username:any , password:any){
@@ -54,45 +56,8 @@ this.toster.success('Logged In Successfully')
           } ,err=>{ 
           this.toster.error(err.message.err.status);
           })
-=======
-  
-  constructor(private home :HomeService, 
->>>>>>> fd181e97a6fe964a2028f4fc08696f5026a77eb0
     
-    private toastr:ToastrService,private router:Router,private http:HttpClient) { }
-  submit(username:any, password:any)
-  {
-    var body ={
-      username:username.value.toString(),
-      password: password.value.toString()
-    }
-    const headerDic={
-      'Content-Type' :'application/json',
-      'Accept':'application/json'
-    }
-    const requestOptions={
-      headers: new HttpHeaders(headerDic)
-    }
-    this.http.post('https://localhost:44352/api/JWT/auth/',body,requestOptions).subscribe((resp:any)=>{
-      const responce={
-        token :resp.toString()
-      }
-      localStorage.setItem('token',responce.token);
-      let data :any=jwt_decode(responce.token);
-      localStorage.setItem('user',JSON.stringify({...data}));
-      console.log(data)
-      if(data.role=="admin")
-      this.router.navigate(['']);
-      else if (data.role=='user')
-      this.router.navigate(['home/']);
-      console.log(data.role)
-
-      
-
-    },err=>{
-      this.toastr.error(err.message,err.status);
-    })
-  }
+   }
 }
          
 // console.log("hello");
